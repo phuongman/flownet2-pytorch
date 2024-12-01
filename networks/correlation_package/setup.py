edb448075adc9c3ccdf7f7b9825b419c5051e74a
22021119_Man Thi Bich Phuong <124645869+phuongman@users.noreply.github.com>
2024-12-01 13:46:36 +0700

    Update setup.py

--- networks/correlation_package/setup.py
+++ networks/correlation_package/setup.py
@@ -1,28 +1,20 @@
 #!/usr/bin/env python3
 import os
 import torch
-
-from setuptools import setup, find_packages
-from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+from setuptools import setup
+from torch.utils.cpp_extension import BuildExtension, CppExtension  # Thay CUDAExtension bằng CppExtension
 
 cxx_args = ['-std=c++11']
 
-nvcc_args = [
-    '-gencode', 'arch=compute_50,code=sm_50',
-    '-gencode', 'arch=compute_52,code=sm_52',
-    '-gencode', 'arch=compute_60,code=sm_60',
-    '-gencode', 'arch=compute_61,code=sm_61',
-    '-gencode', 'arch=compute_70,code=sm_70',
-    '-gencode', 'arch=compute_70,code=compute_70'
-]
+# Xóa nvcc_args vì không sử dụng CUDA
 
 setup(
-    name='correlation_cuda',
+    name='correlation_cpu',  # Đổi tên nếu cần
     ext_modules=[
-        CUDAExtension('correlation_cuda', [
-            #'correlation_cuda.cc',
-            'correlation_cuda_kernel.cu'
-        ], extra_compile_args={'cxx': cxx_args, 'nvcc': nvcc_args})
+        CppExtension('correlation_cpu', [  # Chuyển sang sử dụng CppExtension
+            #'correlation_cuda.cc',  # Loại bỏ tệp CUDA
+            'correlation_cuda_kernel.cpp'  # Sử dụng tệp C++ thay vì tệp CUDA
+        ], extra_compile_args={'cxx': cxx_args})  # Không cần nvcc_args nữa
     ],
     cmdclass={
         'build_ext': BuildExtension
